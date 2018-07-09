16d71b652ca554b3d3293253d3efb76333ff3259
fathimathuzahra j <38877176+FATHI15@users.noreply.github.com>
2018-07-09 11:25:02 +0530

    Add files via upload

---
+++ stockread.py
@@ -0,0 +1,5 @@
+stock1=raw_input("Enter the  name of the stock1")
+stock2=raw_input("Enter the name of the stock2")
+stock3=raw_input("Enter the name of the stock3")
+stock4=raw_input("Enter the name of the stock4")
+print stock1,stock2,stock3,stock4
